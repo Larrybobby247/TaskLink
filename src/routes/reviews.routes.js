bb56b6cd9a7c57b0c9118fc000a867bdeebe7ee0
Larrybobby247 <168409573+Larrybobby247@users.noreply.github.com>
2026-09-24 10:19:10 +0100

    Add routes for order review and create review

--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -4,5 +4,7 @@ import * as reviewsController from '../controllers/reviews.controller.js';
 const router = Router();
 
 router.get('/user/:userId', reviewsController.getUserReviews);
+router.get('/order/:orderId', reviewsController.getOrderReview);
+router.post('/', reviewsController.createReview);
 
 export default router;
