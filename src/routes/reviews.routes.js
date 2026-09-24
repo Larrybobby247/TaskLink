bab4b888bfe0dca2adb01584a18665f5101cee38
Larrybobby247 <168409573+Larrybobby247@users.noreply.github.com>
2026-09-24 11:41:03 +0100

    Remove unused routes from reviews.routes.js

--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -4,7 +4,5 @@ import * as reviewsController from '../controllers/reviews.controller.js';
 const router = Router();
 
 router.get('/user/:userId', reviewsController.getUserReviews);
-router.get('/order/:orderId', reviewsController.getOrderReview);
-router.post('/', reviewsController.createReview);
 
 export default router;
